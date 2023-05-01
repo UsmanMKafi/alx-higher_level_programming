d62b43ee50c5964a1ccf4eb3ef98abad8c2330cc
UsmanMKafi <mkafi3000@gmail.com>
2023-05-01 09:50:31 -0700

    file update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    '''
-    Replaces or adds key/value in a dictionary
-    Parameters:
-    a_dictionary (dict): The dictionary to modify
-    key (str): The key to add
-    value (Any): The value of the key
-    Returns:
-    The modified dictionary
-    '''
-    a_dictionary[key] = value
-    return a_dictionary 
+
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict 
